79f302a0d0750af64a3d92a589ab19e5ac35e052
Vashow <muriukiaw@yahoo.com>
2022-08-23 08:47:34 +0000

    Understanding how to use environment variables'

---
+++ Practice/variables.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo 'The current logged-in User is:' $USER
+echo 'Home directory of the current user is:' $HOME
+echo 'Pathname o the current user'"'"'s shell is:' $SHELL
+echo 'Users unique identiier is:' $UID
