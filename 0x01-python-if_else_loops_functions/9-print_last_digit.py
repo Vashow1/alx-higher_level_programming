d5ee4d26f3487527f674b780bfcfef8f0dfb8714
Vashow <muriukiaw@yahoo.com>
2022-08-24 17:34:53 +0000

    Fixing 9-print_last_digit.py to pycodestyle

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     numStr = str(number)
     last_digit = int(numStr[-1])
-    if last_digit >= 0 and last_digit <=9:
+    if last_digit >= 0 and last_digit <= 9:
         print(last_digit, end='')
         return (last_digit)
     else:
