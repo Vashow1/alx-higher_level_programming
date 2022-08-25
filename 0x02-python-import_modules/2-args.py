c1898f93c2255bf9a05a743bb48ecb783b84229e
Vashow <muriukiaw@yahoo.com>
2022-08-25 09:56:49 +0000

    fixing output of2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,9 @@ if __name__ == "__main__":
     num = len(sys.argv) - 1
     print(f"{num} argument", end='')
     if num > 1:
-        for i in num:
-            print(f"s:\n{i}: {sys.argv[i + 1]}")
+        print("s:")
+        for i in range(num):
+            print(f"{i + 1}: {sys.argv[i + 1]}")
     elif num == 1:
         print(f":\n1: {sys.argv[1]}")
     else:
