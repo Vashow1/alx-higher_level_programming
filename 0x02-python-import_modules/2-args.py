4e3debe4050988f4500f2067f173da8d7cc50ffc
Vashow <muriukiaw@yahoo.com>
2022-08-25 10:02:26 +0000

    Using format strings instead of fstrings

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 import sys
 if __name__ == "__main__":
     num = len(sys.argv) - 1
-    print(f"{num} argument", end='')
+    print("{} argument".format(num), end='')
     if num > 1:
         print("s:")
         for i in range(num):
-            print(f"{i + 1}: {sys.argv[i + 1]}")
+            print("{}: {}".format((i + 1), sys.argv[i + 1]))
     elif num == 1:
-        print(f":\n1: {sys.argv[1]}")
+        print(":\n1: {}".format(sys.argv[1]))
     else:
         print("s.")
 
