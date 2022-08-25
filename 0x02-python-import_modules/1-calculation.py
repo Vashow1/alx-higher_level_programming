1f51c2eabb79b87fd8816ff99c9b8a6e60514150
Vashow <muriukiaw@yahoo.com>
2022-08-25 09:01:34 +0000

    Completing 2-args.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
+from calculator_1 import add, sub, div, mul
+a = 10
+b = 5
 print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
