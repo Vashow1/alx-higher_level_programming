6b2038d3b083e3ee505e6c1964e610541410d7aa
Vashow <muriukiaw@yahoo.com>
2022-08-25 15:39:25 +0000

    Making program not execute when imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
-surprises = dir(hidden_4)
-for surprise in surprises:
-    if surprise[0] != '_' and surprise[1] != '_':
-        print(surprise)
+if __name__ == "__main__":
+    import hidden_4
+    surprises = dir(hidden_4)
+    for surprise in surprises:
+        if surprise[0] != '_' and surprise[1] != '_':
+            print(surprise)
 
