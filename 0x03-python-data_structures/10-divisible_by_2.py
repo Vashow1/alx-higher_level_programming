a1d51d6d3c2786cf35795aa1194d2aa544b7422f
Vashow <muriukiaw@yahoo.com>
2022-08-29 19:45:27 +0000

    Complying to pycodestyle

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     conditions = my_list.copy()
-    i = 0 
+    i = 0
     for item in my_list:
         if (item % 2) == 0:
             conditions[i] = True
@@ -9,4 +9,3 @@ def divisible_by_2(my_list=[]):
             conditions[i] = False
         i += 1
     return conditions
-
