be05bc1b8c15fd7270807e5d58233f08e9ad4c15
Vashow <muriukiaw@yahoo.com>
2022-08-29 19:43:34 +0000

    Solving 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    conditions = my_list.copy()
+    i = 0 
+    for item in my_list:
+        if (item % 2) == 0:
+            conditions[i] = True
+        else:
+            conditions[i] = False
+        i += 1
+    return conditions
+
