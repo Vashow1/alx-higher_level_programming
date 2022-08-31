73ed93d82ffb8cacf64a185ee3503c71d7081968
Vashow <muriukiaw@yahoo.com>
2022-08-31 22:33:47 +0000

    Pycodestyle

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None or len(a_dictionary) == 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     big_num = 0
     for key in a_dictionary:
