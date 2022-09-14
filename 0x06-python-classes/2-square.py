f2f9325c977149d68927b698b07ca6a42d2449d6
Vashow <muriukiaw@yahoo.com>
2022-09-14 18:12:44 +0000

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Demonstrate validation"""
+
+
+class Square:
+    """A class representation of a square shape"""
+
+    def __init__(self, size=0):
+        """initializing the square"""
+        self.__size = size
+        if (isinstance(size, int)):
+            pass
+        else:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
