6f0727d498a50ba902fad0a4e6c86031d982d2eb
Vashow <muriukiaw@yahoo.com>
2022-09-28 17:09:20 +0000

    Can't see how the test got 29 from 24 characters written

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+"""Opens and writes text to a file"""
+
+def write_file(filename="", text=""):
+    """Opens filename and writes text into it"""
+    with open(filename, mode='w', encoding='utf-8') as a_file:
+        a_file.write(text)
+    return (len(text))
