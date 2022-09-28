1727a4c54e613a63e62caf2f969bed0d54c7b499
Vashow <muriukiaw@yahoo.com>
2022-09-28 17:23:02 +0000

    python docstrings

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,8 @@
 
 
 def append_write(filename="", text=""):
+    """Opens filename and appends text to it"""
+
     with open(filename, mode='a', encoding='utf-8') as a_file:
         a_file.write(text)
     return (len(text))
