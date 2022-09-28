c5b64620c79ef6a962197e0d19fb4b3fd116f1db
Vashow <muriukiaw@yahoo.com>
2022-09-28 22:42:52 +0000

    working out 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script containing a class that defines a student"""
+
+
+class Student:
+    """Definition of the class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialises the student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves the json rep of the instance"""
+        if attrs is None:
+            return (self.__dict__)
+        attrs2 = {x: self.__dict__[x] for x in attrs if x in self.__dict__}
+        return (attrs2)
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
