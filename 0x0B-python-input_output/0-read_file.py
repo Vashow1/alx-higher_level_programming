184f63c206aa8538fe4a5825502a11b2cc6e46ea
Vashow <muriukiaw@yahoo.com>
2022-09-28 16:55:34 +0000

    Adding documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Creates a function that prints contents of a file"""
+
+
 def read_file(filename=""):
+    """Opens a file and reads into it"""
     with open(filename, mode='r', encoding='utf-8') as a_file:
         print(a_file.read())
