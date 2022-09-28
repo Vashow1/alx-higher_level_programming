5b7d5517a59b8edd70d2cf71f1b9da50dc40a01b
Vashow <muriukiaw@yahoo.com>
2022-09-28 16:59:17 +0000

    Fixing errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Opens a file and reads into it"""
     with open(filename, mode='r', encoding='utf-8') as a_file:
-        print(a_file.read())
+        print(a_file.read(), end="")
