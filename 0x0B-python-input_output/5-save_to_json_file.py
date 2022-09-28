565f4758a0e1b96017bc5ab5749894d09dfadc88
Vashow <muriukiaw@yahoo.com>
2022-09-28 18:10:24 +0000

    Fixing errors

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
-    jsonrep = json.dumps(my_obj)
     with open(filename, mode='w', encoding='utf-8') as a_file:
-        a_file.write(jsonrep)
+        json.dump(my_obj, a_file)
