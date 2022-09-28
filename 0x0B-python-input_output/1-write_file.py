1e7b67d89931d87d9b1a3888af75c93401922ae1
Vashow <muriukiaw@yahoo.com>
2022-09-28 17:16:17 +0000

    pycodestyle

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Opens and writes text to a file"""
 
+
 def write_file(filename="", text=""):
     """Opens filename and writes text into it"""
     with open(filename, mode='w', encoding='utf-8') as a_file:
