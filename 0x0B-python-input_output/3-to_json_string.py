76e9854d3e275a3d6990c4ddfc56a7b62aa956aa
Vashow <muriukiaw@yahoo.com>
2022-09-28 17:38:47 +0000

    Creating 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""converts stringt to json rep"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+    stringjson = json.dumps(my_obj)
+    return (stringjson)
