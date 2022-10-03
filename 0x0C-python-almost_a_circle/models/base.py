d9f8bba8a14525780eea0867c41a1f8fbc55dfbf
Vashow <muriukiaw@yahoo.com>
2022-10-03 22:48:54 +0000

    File to instances & Dictionary to Instance  & JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,35 @@ class Base:
             else:
                 list_dict = [obj.to_dictionary() for obj in list_objs]
                 a_file.write(Base.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None:
+            return []
+        else:
+            return (json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        returns an instance with all attributes already set based on **dictionary
+        note size, width and length must be > 0 hence the dummy values used
+
+        """
+        if dictionary != {} and len(dictionary) != 0:
+            if cls.__name__ == "Rectangle":
+                new_instance = cls(1, 1)
+            else:
+                new_instance = cls(1)
+            new_instance.update(**dictionary)
+            return new_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances:"""
+        filename = cls.__name__ + ".json"
+
+        with open(filename, "r") as a_file:
+            list_dict = Base.from_json_string(a_file.read())
+            return [cls.create(**dictionary) for dictionary in list_dict]
