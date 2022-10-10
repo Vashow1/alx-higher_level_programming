a5b1c0065f3d89a1ccc71951a6ee18c2040baf5f
Vashow <muriukiaw@yahoo.com>
2022-10-10 06:36:43 +0000

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function that checks whether instance if from class
+that inherited from a super class
+
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class;
+    otherwise False
+
+    """
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
