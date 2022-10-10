2c0e2876edc3f55bcd22672ba27921e05472fc5a
Vashow <muriukiaw@yahoo.com>
2022-10-10 06:48:46 +0000

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module defines the class base_geometry"""
+
+
+class BaseGeometry:
+    """Preliminary declaration of class"""
+    
+    def area(self):
+        """Area of the base geometry"""
+
+        raise Exception("area() is not implemented")
