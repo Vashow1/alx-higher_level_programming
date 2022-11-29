2ed3d6fd7b2b4c12d755c52ca769f8cd2861bc19
Vashow1 <muriukiaw1@gmail.com>
2022-11-29 21:45:57 +0000

    Writing code showing returning a function from a function and also the global footprint of variables in javascript

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-	let i = list.length - 1;
-	let j = 0;
-	let reversed = []
-	for (i, j; list[i]; i--, j++) {
-		reversed[j] = list[i];
-	}
-	return (reversed);
-}
+  let i = list.length - 1;
+  let j = 0;
+  const reversed = [];
+  for (i, j; list[i]; i--, j++) {
+    reversed[j] = list[i];
+  }
+  return (reversed);
+};
