abde5906253e88d453e74630ca0153405ad5e0ec
Vashow1 <muriukiaw1@gmail.com>
2022-11-29 20:00:49 +0000

    Making the file compliant to semistandard

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  occurences = 0;
+  let occurences = 0;
   for (let i = 0; list[i]; i++) {
     if (searchElement === list[i]) {
       occurences++;
