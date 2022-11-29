16a71776302fb08c5dcf161ed9edf1dc21a399a4
Vashow1 <muriukiaw1@gmail.com>
2022-11-29 19:27:26 +0000

    Extending previous square instead

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 
 module.exports = class Square extends require('./5-square') {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       super.print();
