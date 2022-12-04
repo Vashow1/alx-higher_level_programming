a5881de1b0a9be6ee4589aaafb15c113bac8c001
Vashow1 <muriukiaw1@gmail.com>
2022-12-04 07:45:51 +0000

    Using where and like in mysqldb

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+"""
+lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+    MY_HOST = "localhost"
+    MY_DB = sys.argv[3]
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS,
+                         db=MY_DB)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE `name` LIKE "N%"'
+                + ' ORDER BY `id` ASC')
+    for row in cur.fetchall():
+        print(row)
