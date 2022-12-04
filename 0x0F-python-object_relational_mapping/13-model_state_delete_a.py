e712437c8f3e558e21957ae0340b6b62f6befdd0
Vashow1 <muriukiaw1@gmail.com>
2022-12-04 22:46:14 +0000

    Failing one test

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -11,13 +11,12 @@ if __name__ == "__main__":
     user = sys.argv[1]
     passw = sys.argv[2]
     host = "localhost"
-    port = 3306
     data = sys.argv[3]
     engine = create_engine(f'mysql+mysqldb://{user}:{passw}@{host}/{data}')
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        if 'a' in state.name or 'A' in state.name:
+    for state in session.query(State):
+        if 'a' in state.name:
             session.delete(state)
     session.commit()
