7574bef5b20079512fd3ac57a6e2435a02e005c4
Vashow1 <muriukiaw1@gmail.com>
2022-12-04 21:35:25 +0000

    Filter by A

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database
+hbtn_0e_6_usa using SQLAlchemy
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passw = sys.argv[2]
+    host = "localhost"
+    port = 3306
+    data = sys.argv[3]
+    engine = create_engine(f'mysql+mysqldb://{user}:{passw}@{host}/{data}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        if 'a' in state.name or 'A' in state.name:
+            print(f"{state.id}: {state.name}")
