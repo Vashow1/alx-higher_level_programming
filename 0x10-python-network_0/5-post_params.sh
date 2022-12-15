ad9d40c0ac3f8e9712b19f566bba9ded2de70b68
Vashow1 <muriukiaw1@gmail.com>
2022-12-15 22:57:22 +0000

    forgetting

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL, using specified params
-curl -s -d "e-mail=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -XPOST -d "e-mail=test@gmail.com&subject=I will always be here for PLD" "$1"
