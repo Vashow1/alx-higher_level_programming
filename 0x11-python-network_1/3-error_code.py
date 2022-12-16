008836a249b0754ee3a24906e87e5eef1e374e92
Vashow1 <muriukiaw1@gmail.com>
2022-12-16 20:34:32 +0000

    handling HTTPErrors

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and
+displays the body of the response
+The essence of this exercise is to practice on HTTP error handling
+"""
+import urllib
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read()
+        print(content.decode('utf-8'))
+
+    except urllib.error.HTTPError as error:
+        print("Error code:", error.status)
